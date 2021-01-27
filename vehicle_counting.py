bac552bcb36a6a3a13451494dfee9aa796607353
Aditya Solanki <aditya2ss283@gmail.com>
2021-01-28 02:33:10 +0530

    testing on images (2/5)

--- vehicle_counting.py
+++ vehicle_counting.py
@@ -27,9 +27,10 @@ import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-im = cv2.imread('test4.jpg')
+im = cv2.imread("images/traffic4.jpg")
 bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
 plt.show()
-print('Number of cars in the image is '+ str(label.count('car')))
+print('Number of Vehicles in the image is '+ str(label.count('car') + label.count('bus') + label.count('motorcycle') + label.count('person') + label.count('truck') ))
+##adding person since it considers motobikes as 'person'
